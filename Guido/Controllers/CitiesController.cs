e96449c82f3ee2f97f3cf80918a2caff2d027538
marija1408 <marijad14@gmail.com>
2015-05-08 13:40:35 +0200

    stavljanjee projekta

---
+++ Guido/Controllers/CitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Baza;
+
+namespace Guido.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private GuidoEntities db = new GuidoEntities();
+
+        // GET: /Cities/
+        public ActionResult Index()
+        {
+            var city = db.City.Include(c => c.State);
+            return View(city.ToList());
+        }
+
+        // GET: /Cities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.City.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // GET: /Cities/Create
+        public ActionResult Create()
+        {
+            ViewBag.fk_State = new SelectList(db.State, "ID", "stateName");
+            return View();
+        }
+
+        // POST: /Cities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,fk_State,cityName")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.City.Add(city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.fk_State = new SelectList(db.State, "ID", "stateName", city.fk_State);
+            return View(city);
+        }
+
+        // GET: /Cities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.City.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.fk_State = new SelectList(db.State, "ID", "stateName", city.fk_State);
+            return View(city);
+        }
+
+        // POST: /Cities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,fk_State,cityName")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(city).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.fk_State = new SelectList(db.State, "ID", "stateName", city.fk_State);
+            return View(city);
+        }
+
+        // GET: /Cities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.City.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // POST: /Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            City city = db.City.Find(id);
+            db.City.Remove(city);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
